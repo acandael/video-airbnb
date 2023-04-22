35a70f8c12761e35a98f1a934ecb12b1916c1442
Anthony Candaele <anthony.candaele@tothepoint.company>
2023-04-22 15:56:07 +0200

    add all the rest

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import Image from "next/image";
+
+interface AvatarProps {
+  src: string | null | undefined
+}
+
+const Avatar: React.FC<AvatarProps> = ({src}) => {
+  return (
+    <Image src={src || "/images/placeholder.jpg"} className="rounded-full" alt="Avatar" width="30" height="30" />
+  )
+}
+
+export default Avatar
